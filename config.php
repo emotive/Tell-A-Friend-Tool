ad6e3c02b247480822ec5681d9aa084b197f9c25
kduncan <kduncan@emotivellc.com>
2010-11-04 15:59:31 -0400

    updated index.php and config.php files to make tool generic

--- config.php
+++ config.php
@@ -29,7 +29,7 @@ $CUR_PAGE = 1;
 //Site title
 $EMAIL_OPTIONS['TITLE'] = $_POST['first_name'].' '.$_POST['last_name'];
 //Site URL
-$EMAIL_OPTIONS['URL'] = 'http://www.RobPortman.com';
+$EMAIL_OPTIONS['URL'] = $_POST['this_page'};
 //Main email
 $EMAIL_OPTIONS['FROM'] = $_POST['email'];
 //Charset 
@@ -37,7 +37,7 @@ $EMAIL_OPTIONS['CHARSET'] = 'utf-8';
 //Type -> HTML=text/html | Text = text/plain
 $EMAIL_OPTIONS['TYPE'] = 'text/html';
 //Email subjects
-$EMAIL_OPTIONS['EMAIL_SUBJECT'] = 'Join me and Vote for Rob Portman';
+$EMAIL_OPTIONS['EMAIL_SUBJECT'] = 'Your email subject goes here';
 
 /*
 For Templates you can use the following variables:
@@ -48,7 +48,16 @@ For Templates you can use the following variables:
 
 //EMAIL Template
 
-$EMAIL_TEMPLATE = '<html><head><title>Rob Portman for US Senate</title></head><body><p>Please join me in taking a pledge to vote on November 2, 2010.</p><p>Ohio is in trouble, and Columbus and Washington just don\'t get it. Ohio has had enough of Lee Fisher\'s failed policies. We must do everything possible to make sure our message reaches every voter. That is why I need your help, especially now, in this home stretch. Ohio needs someone who will focus on jobs for Ohioans, not the politics that are leading us down the wrong track. This election is about helping real Ohioans; about getting Ohio back on a path to prosperity. That is why I am voting for Rob Portman.</p><p>Please visit http://www.RobPortman.com to join me and sign up for voting information. Help spread the news to your friends and family by following this link http://www.RobPortman.com/.</p></body></html>';
+$EMAIL_TEMPLATE = '<html>
+<head>
+<title></title>
+</head>
+<body>
+<p>$+name+$</p>
+<p>$+message+$</p>
+<p>$+message_text+$</p>
+</body>
+</html>';
 
 /* +++++++++++++++++++++++++++++++++++++
 		END FILE CONFIGURATION
--- index.php
+++ index.php
@@ -1,68 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" dir="ltr">
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<head>
 <script type="text/javascript" src="https://www.plaxo.com/css/m/js/util.js"></script>
 <script type="text/javascript" src="https://www.plaxo.com/css/m/js/basic.js"></script>
 <script type="text/javascript" src="https://www.plaxo.com/css/m/js/abc_launcher.js"></script>
-<title>Join Team Portman | Rob Portman for U.S. Senate</title>
-<meta name="description" content=" The Official Site of Rob Portman for Senate | Ohio 2010">
-<meta name="keywords" content=" Rob Portman, U.S. Senate, Portman for Senate, Ohio 2010, Senate 2010, Portman Campaign, Ohio Senate,Join Team Portman">
-<meta name="reply-to" content="contact@robportman.com">
-<meta name="copyright" content="Copyright 2010">
-<meta http-equiv="content-language" content="en">
-<meta name="rating" content="General">
-<meta name="verify-v1" content="" />
-<meta name="robots" content="index, follow">
-<meta name="robots" content="archive" />
-<meta name="googlebot" content="archive">
-<title>Pledge to Vote</title>
 <style type="text/css">
-body {
-background:url("../images/bg.jpg") repeat-x scroll center top #006699;
-}
-#splash {
-	background:url(portman_splash_bg.png);
-	width:901px;
-	height:635px;
-	margin:0 auto;
-	font-family:Arial, Helvetica, sans-serif;
-	font-size:16px;
-}
-.splash_header {
-	width:901px;
-	margin-bottom:20px;
-}
-.splashlogo {
-	width:344px;
-	float:left;
-	display:inline;
-	padding-top:15px;
-	text-align:right;
-}
-.splashtitle {
-	padding-top:21px;
-	padding-left:31px;
-	width:500px;
-	float:left;
-	display:inline;
-}
-.splashcontent {
-	padding:81px 42px;
-}
-.splashtitle h1 {
-	margin:0;
-	font-size:21px;
-	color:#06699b;
-}
 .splashinput {
 	width:155px;
 	height:32px;
 	margin-right:8px;
 	border:1px solid black;
 	font-size:20px;
-	/*padding:22px 20px;*/
 }
 .splashemail {
 	margin-top:10px;
@@ -86,9 +31,6 @@ textarea {
 	border:none;
 	cursor:pointer;
 	margin-top:10px;
-}
-#skip_link {
-color:white;	
 }
 </style>
 <?php
@@ -184,18 +126,7 @@ if($_POST['submit_id'] == 1){
 	}
 }
 ?>
-</head>
-<body>
-<div id="splash">
-<div class="splash_header">
-  <div class="splashtitle">
-    <h1>Join Me and Pledge to Vote on Election Day</h1>
-  </div>
-  <div class="splashlogo"><a href="#"><img src="portman_splash_logo.png" width="144" height="46" border="0" /></a></div>
-</div>
-<div class="splashcontent">
-<p>Help Rob Portman send Ohio in a new direction.  Please <strong>invite as many friends as possible</strong> to the polls on November 2nd to vote.   Click the <strong>&quot;Add Your Friends&quot;</strong> button to access your address book and send  a message to remind your friends, family, and neighbors to vote in this critical election.</p>
-<p>Thank you for your support. </p>
+</head><body>
 <?php
 if($alert_text != NULL){
 ?>
@@ -207,39 +138,39 @@ if($alert_text != NULL){
   </tr>
 </table>
 <?php } ?>
-  <form name="signup" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-    <table width="846" cellpadding="10" cellspacing="0" border="0" align="center">
-      <tr>
-        <td width="50%" valign="top"><a title="Click here to invite your friends" href="#" onclick="showPlaxoABChooser('toemails', '/splash/plaxo_cb.html'); return false"><img src="addbutton.jpg" width="343" height="138" border="0" /></a>
-          <h3>Or type their email addresses in the box below:</h3>
-          <textarea style="margin:0;" name="toemails" id="toemails" cols="30" rows="7"></textarea></td>
-        <td valign="top" style="padding-left:40px;"><input name="first_name" type="text" value="First Name" class="splashinput" onblur="if(this.value=='')this.value='First Name'" onfocus="if(this.value=='First Name')this.value=''" />
-          <input name="last_name" type="text" value="Last Name" class="splashinput" onblur="if(this.value=='')this.value='Last Name'" onfocus="if(this.value=='Last Name')this.value=''" />
-          <input name="email" type="text" value="Email Address" class="splashemail" onblur="if(this.value=='')this.value='Email Address'" onfocus="if(this.value=='Email Address')this.value=''" />
-          <h3>Below is the message that will be sent to your friend(s)</h3>
-          <textarea name="message_text" cols="30" rows="7" readonly="readonly">Please join me in taking a pledge to vote on November 2, 2010.
-
-Ohio is in trouble, and Columbus and Washington just don't get it. Ohio has had enough of Lee Fisher's failed policies. We must do everything possible to make sure our message reaches every voter. That is why I need your help, especially now, in this home stretch. Ohio needs someone who will focus on jobs for Ohioans, not the politics that are leading us down the wrong track. This election is about helping real Ohioans; about getting Ohio back on a path to prosperity. That is why I am voting for Rob Portman.
-
-Please visit http://www.RobPortman.com to join me and sign up for voting information. Help spread the news to your friends and family by following this link http://www.RobPortman.com.
-</textarea>
-          <input name="submit_id" type="hidden" id="submit_id" value="1">
-          <input name="this_page" type="hidden" id="this_page" value="<?=$_POST['this_page'];?>">
-          <br />
-          <input name="" value="" type="submit" class="splashinvite" onclick="document.signup.toemails.value=findEmailAddresses(document.signup.toemails.value);" />
-          </td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-      </tr>
-    </table>
-  </form>
-</div>
-</div>
-<div align="center"><a href="http://www.robportman.com" id="skip_link">
-  <p>Skip to the main page &raquo;
-  </a></div>
+<form name="signup" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+  <table width="846" cellpadding="10" cellspacing="0" border="0" align="center">
+    <tr>
+      <td width="50%" valign="top"><a title="Click here to invite your friends" href="#" onclick="showPlaxoABChooser('toemails', 'plaxo_cb.html'); return false"><img src="addbutton.jpg" width="343" height="138" border="0" /></a>
+        <h3>Or type their email addresses in the box below:</h3>
+        <textarea style="margin:0;" name="toemails" id="toemails" cols="30" rows="7"></textarea></td>
+      <td valign="top" style="padding-left:40px;"><table cellpadding="5" cellspacing="0" border="0">
+          <tr>
+            <td><input name="first_name" type="text" value="First Name" class="splashinput" onblur="if(this.value=='')this.value='First Name'" onfocus="if(this.value=='First Name')this.value=''" /></td>
+            <td><input name="last_name" type="text" value="Last Name" class="splashinput" onblur="if(this.value=='')this.value='Last Name'" onfocus="if(this.value=='Last Name')this.value=''" /></td>
+          </tr>
+          <tr>
+            <td colspan="2"><input name="email" type="text" value="Email Address" class="splashemail" onblur="if(this.value=='')this.value='Email Address'" onfocus="if(this.value=='Email Address')this.value=''" /></td>
+          </tr>
+          <tr>
+            <td colspan="2"><h3>Below is the message that will be sent to your friend(s)</h3>
+              <textarea name="message_text" cols="30" rows="7">Enter your message here. You can make it a read-only message by adding the readonly attribute to the textarea</textarea></td>
+          </tr>
+          <?php
+	if($REQUIRE_MESS == 1){?>
+          <tr>
+            <td colspan="2"><textarea name="message_text" cols="30" rows="7" id="message_text">You can also require the user to enter their own message here.</textarea></td>
+          </tr>
+          <?php } ?>
+          <tr>
+            <td colspan="2"><input name="submit_id" type="hidden" id="submit_id" value="1">
+              <input name="this_page" type="hidden" id="this_page" value="<?=$_POST['this_page'];?>">
+              <input name="" value="" type="submit" class="splashinvite" onclick="document.signup.toemails.value=findEmailAddresses(document.signup.toemails.value);" /></td>
+          </tr>
+        </table></td>
+    </tr>
+  </table>
+</form>
 <script type="text/javascript">
 function findEmailAddresses(StrObj) {
 var separateEmailsBy = ", ";
